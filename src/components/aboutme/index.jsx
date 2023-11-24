fa81dd9b9393ce0240342c31b3fcbd913c1507f7
Kyndallmae <kyndallmae@gmail.com>
2023-11-24 10:48:56 -0600

    modified img name and indexcss, added aboutme

---
+++ src/components/aboutme/index.jsx
@@ -0,0 +1,31 @@
+function About() {
+    return (
+      <div className="row mx-3 py-5 about">
+        <div className="col-12 col-lg-4 text-center animate__animated animate__fadeInLeftBig">
+          <img
+            className="profile-pic"
+            src="./img/profilepic.jpeg"
+            alt="Profile Pic"
+          />
+        </div>
+        <div className="about-me col-12 col-lg-8 px-5 my-5 animate__animated animate__fadeInLeft">
+          <h1 className="display-4 pb-1">About Me</h1>
+          <p className="about-p">
+          Teacher-turned-Web Developer! As a driven and tech-savvy web developer, 
+          I have always had a strong gravitational pull toward technology. 
+          I am currently pursuing an intensive web development coding certificate at Southern Methodist University. 
+          I am gaining experience in the foundations of web development while honing my skills in HTML5, CCS, Bootstrap, JavaScript, jQuery, 
+          and a bit of Python and MySQL.
+          </p>
+          <p className="about-p">
+          Having an appetite for continuous learning, collaboration, and problem-solving, 
+          I plan to create and maintain websites while also developing iOS and Android apps.
+          Equally passionate about building applications that assist people to make their lives easier. 
+          Looking to bring my attention to detail and technical skills to a company to grow and become a valuable asset.
+          </p>
+        </div>
+      </div>
+    );
+  }
+  
+  export default About;
---
+++ src/components/contactinfo/index.jsx
@@ -0,0 +1,73 @@
+import React, { useState } from "react";
+import emailjs from "emailjs-com";
+
+function Contact() {
+
+  const [ contactName, setContactName ] = useState('')
+  const [ contactEmail, setContactEmail ] = useState('')
+  const [ contactSubject, setContactSubject ] = useState('')
+  const [ contactMessage, setContactMessage ] = useState('')
+
+  function sendEmail(e) {
+    e.preventDefault();
+
+    emailjs
+      .sendForm("service_gd267mb", "template_4iik70o", e.target, "KJh3qHLSfN9B91nQU")
+      .then(
+        (result) => {
+          console.log('success')
+          setContactName('')
+          setContactEmail('')
+          setContactSubject('')
+          setContactMessage('')
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      )
+      .then(
+        document
+          .querySelector('#response-message')
+          .classList.remove('d-none')
+      )
+  }
+
+  return (
+    <div className="container mb-4 pb-5">
+      <div className="row justify-content-center">
+        <div className="col-6 col-lg-3">
+          <h1 className="display-2 text-white pb-3">
+            Contact
+          </h1>
+        </div>
+      </div>
+      <div className="row py-4 justify-content-center">
+        <form className="contact-card col-11 col-lg-6" onSubmit={sendEmail}>
+          <div className="card-body mb-3">
+            <label className="form-label contact-form">Name</label>
+            <input type="text" className="form-control" id="contact-name" name="from_name" value={contactName} onChange={(e) => setContactName(e.target.value)} />
+            <label className="form-label contact-form pt-3">Email address</label>
+            <input type="email" className="form-control" id="contact-email" name="from_email" value={contactEmail} onChange={(e) => setContactEmail(e.target.value)} />
+            <label className="form-label pt-3 contact-form">Subject</label>
+            <input type="text" className="form-control" id="contact-subject" name="subject" value={contactSubject} onChange={(e) => setContactSubject(e.target.value)} />
+          </div>
+          <div className="mb-3">
+            <label className="form-label contact-form">Message</label>
+            <textarea
+              className="form-control"
+              id="contact-message"
+              value={contactMessage} 
+              onChange={(e) => setContactMessage(e.target.value)}
+              name="message"
+              rows="5"
+            ></textarea>
+            <button className="btn btn-primary mt-3 float-end" type="submit" value="Send">Submit</button>
+          </div>
+        </form>
+        <p className="d-none" id="response-message">Message Sent. I will be in contact soon!</p>
+      </div>
+    </div>
+  );
+}
+
+export default Contact;
