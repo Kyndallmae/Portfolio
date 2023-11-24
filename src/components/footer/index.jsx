c7447f54ff244975beba721834b3c5b6b9c8cb1a
Kyndallmae <kyndallmae@gmail.com>
2023-11-24 10:53:35 -0600

    added photos to footer

---
+++ src/components/footer/index.jsx
@@ -0,0 +1,15 @@
+function Footer() {
+    return (
+      <div className="footer row pt-5">
+        <div className="col-4 mx-auto text-center pt-4">
+          <p>Made with love by Kyndall Carothers</p>
+        </div>
+        <div className="col-7 mx-auto d-flex justify-content-end pt-4">
+          <a href="https://github.com/Kyndallmae"><img className="icon mx-2 mt-1" src="./img/githubicon.png" /></a>
+          <a href="https://linkedin.com/in/kyndall-carothers-a33bb9177"><img className="icon-2 mx-2" src="./img/llinkedinicon.png" /></a>
+        </div>
+      </div>
+    );
+  }
+  
+  export default Footer;
